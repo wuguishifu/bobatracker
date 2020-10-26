132e5159ab5ac9e4926fd109b8239d3afed1051d
Bo Bramer <bramer.bo@gmail.com>
2020-10-25 21:59:09 -0700

    Redesign of the entire UI/UX for the login/signup screen

--- app/src/main/java/com/example/bobatrackerv001/MainActivity.java
+++ app/src/main/java/com/example/bobatrackerv001/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.bobatrackerv001;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,74 +21,35 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference database;
-    private static String user_password;
+    private static String password;
 
+    private static boolean passwordIsShown;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        //buttons
-        Button login = (Button) findViewById(R.id.login);
-        Button signUp = (Button) findViewById(R.id.signUp);
 
-        //text boxes
-        EditText usernameTextField = (EditText) findViewById(R.id.editTextTextPersonName);
-        EditText passwordTextField = (EditText) findViewById(R.id.editTextTextPassword2);
+        Button showPassword = findViewById(R.id.button_show_password);
+        passwordIsShown = false;
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String username = usernameTextField.getText().toString();
-                String password = passwordTextField.getText().toString();
-
-                System.out.println(username);
-                System.out.println(password);
-
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users/" + username + "/pwd");
-                String pass = getPasswordOfUser(username, ref);
-
-
-                if (pass != null && pass.equals(password)) {
-                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                }
+        showPassword.setOnClickListener(v -> {
+            passwordIsShown = !passwordIsShown;
+            EditText passwordTextEntry = findViewById(R.id.password_entry);
+            if (passwordIsShown) {
+                passwordTextEntry.setTransformationMethod(null);
+                showPassword.setText(R.string.hide_password_button);
+            } else {
+                passwordTextEntry.setTransformationMethod(new PasswordTransformationMethod());
+                showPassword.setText(R.string.show_password_button);
             }
+            passwordTextEntry.setSelection(passwordTextEntry.getText().length());
         });
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
 
-                database = FirebaseDatabase.getInstance().getReference();
 
-            }
-        });
-    }
-
-    private void writeNewUser(String id, String password) {
-        Visits visits = new Visits();
-        User user = new User(id, password, visits);
-        database.child("users").child(id).setValue(user);
-    }
-
-    private String getPasswordOfUser(String uid, DatabaseReference ref) {
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.getValue() != null) {
-                    user_password = snapshot.getValue().toString();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        System.out.println(user_password);
-        return user_password;
     }
 }
