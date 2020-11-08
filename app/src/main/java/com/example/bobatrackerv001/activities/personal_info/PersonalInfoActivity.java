a873645132790c7e46670d15faa099cdb0c4d151
Bo Bramer <bramer.bo@gmail.com>
2020-11-07 19:30:48 -0800

    Began writing the personal info activity

---
+++ app/src/main/java/com/example/bobatrackerv001/activities/personal_info/PersonalInfoActivity.java
@@ -0,0 +1,21 @@
+package com.example.bobatrackerv001.activities.personal_info;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bobatrackerv001.R;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    private String username;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+
+    }
+
+}
